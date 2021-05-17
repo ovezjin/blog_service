29c7e991ff88a48e1de4ec94b7f2ba87f8c96470
zzzum <ovezjin@outlook.com>
2021-05-17 18:23:57 +0800

    swag

--- internal/routers/router.go
+++ internal/routers/router.go
@@ -2,13 +2,18 @@ package routers
 
 import (
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-tour-book/blog-service/docs"
 	v1 "github.com/go-tour-book/blog-service/internal/routers/api/v1"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
 )
 
 func NewRouter() *gin.Engine {
 	r := gin.New()
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
+	url := ginSwagger.URL("http://127.0.0.1:8000/swagger/doc.json")
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, url))
 
 	article := v1.NewArticle()
 	tag := v1.NewTag()
--- main.go
+++ main.go
@@ -30,8 +30,12 @@ func init() {
 	}
 }
 
+// @title 博客系统
+// @version 1.0
+// @description Go 语言编程之旅：一起用 Go 做项目
+// @termsOfService https://github.com/go-tour-book
 func main() {
-	global.Logger.Infof("%s: go-programming-tour-book/%s", "eddycjy", "blog-service")
+	// global.Logger.Infof("%s: go-programming-tour-book/%s", "eddycjy", "blog-service")
 	router := routers.NewRouter()
 	s := &http.Server{
 		Addr:           ":" + global.ServerSetting.HttpPort,
