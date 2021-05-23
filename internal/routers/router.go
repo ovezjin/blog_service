f136aed2737351feae2f87eb0b05da82bbec4b71
zzzum <ovezjin@outlook.com>
2021-05-23 12:37:36 +0800

    bind & valid

--- internal/routers/router.go
+++ internal/routers/router.go
@@ -3,6 +3,7 @@ package routers
 import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-tour-book/blog-service/docs"
+	"github.com/go-tour-book/blog-service/internal/middleware"
 	v1 "github.com/go-tour-book/blog-service/internal/routers/api/v1"
 	ginSwagger "github.com/swaggo/gin-swagger"
 	"github.com/swaggo/gin-swagger/swaggerFiles"
@@ -12,6 +13,7 @@ func NewRouter() *gin.Engine {
 	r := gin.New()
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
+	r.Use(middleware.Translations())
 	url := ginSwagger.URL("http://127.0.0.1:8000/swagger/doc.json")
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, url))
 
